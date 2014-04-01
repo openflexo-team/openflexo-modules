4cf4c950d19e942202ded292623a5122c051a3cd
Sylvain Guérin <sylvain@openflexo.org>
2014-04-01 12:02:46 +0200

    MEDIUM / Reactivate unit tests for VPM inspectors

--- flexoviewpointmodeler/src/test/java/org/openflexo/vpm/fib/TestVPMInspectors.java
+++ flexoviewpointmodeler/src/test/java/org/openflexo/vpm/fib/TestVPMInspectors.java
@@ -1,272 +1,248 @@
 package org.openflexo.vpm.fib;
 
-import java.io.File;
-
+import org.junit.Test;
 import org.openflexo.fib.utils.GenericFIBInspectorTestCase;
+import org.openflexo.rm.FileResourceImpl;
+import org.openflexo.rm.ResourceLocator;
 
 public class TestVPMInspectors extends GenericFIBInspectorTestCase {
 
+	/*
+	 * Use this method to print all
+	 * Then copy-paste 
+	 */
+
 	public static void main(String[] args) {
-		System.out.println(generateInspectorTestCaseClass(new File(System.getProperty("user.dir") + "/src/main/resources/Inspectors/VPM"),
+		System.out.println(generateInspectorTestCaseClass(((FileResourceImpl) ResourceLocator.locateResource("Inspectors/VPM")).getFile(),
 				"Inspectors/VPM/"));
 	}
 
+	@Test
 	public void testAbstractActionSchemeInspector() {
 		validateFIB("Inspectors/VPM/AbstractActionScheme.inspector");
 	}
 
+	@Test
 	public void testAbstractCreationSchemeInspector() {
 		validateFIB("Inspectors/VPM/AbstractCreationScheme.inspector");
 	}
 
+	@Test
 	public void testActionSchemeInspector() {
 		validateFIB("Inspectors/VPM/ActionScheme.inspector");
 	}
 
+	@Test
 	public void testCheckboxParameterInspector() {
 		validateFIB("Inspectors/VPM/CheckboxParameter.inspector");
 	}
 
+	@Test
 	public void testClassParameterInspector() {
 		validateFIB("Inspectors/VPM/ClassParameter.inspector");
 	}
 
+	@Test
 	public void testClassPatternRoleInspector() {
-		validateFIB("Inspectors/VPM/ClassRole.inspector");
+		validateFIB("Inspectors/VPM/ClassPatternRole.inspector");
 	}
 
+	@Test
 	public void testCloningSchemeInspector() {
 		validateFIB("Inspectors/VPM/CloningScheme.inspector");
 	}
 
-	public void testConnectorOverridingGraphicalRepresentationInspector() {
-		validateFIB("Inspectors/VPM/ConnectorOverridingGraphicalRepresentation.inspector");
-	}
-
-	public void testConnectorPatternRoleInspector() {
-		validateFIB("Inspectors/VPM/ConnectorPatternRole.inspector");
-	}
-
+	@Test
 	public void testCreationSchemeInspector() {
 		validateFIB("Inspectors/VPM/CreationScheme.inspector");
 	}
 
+	@Test
 	public void testDataPropertyParameterInspector() {
 		validateFIB("Inspectors/VPM/DataPropertyParameter.inspector");
 	}
 
+	@Test
 	public void testDataPropertyPatternRoleInspector() {
-		validateFIB("Inspectors/VPM/DataPropertyRole.inspector");
+		validateFIB("Inspectors/VPM/DataPropertyPatternRole.inspector");
 	}
 
+	@Test
 	public void testDeletionSchemeInspector() {
 		validateFIB("Inspectors/VPM/DeletionScheme.inspector");
 	}
 
-	public void testDiagramModelSlotInspector() {
-		validateFIB("Inspectors/VPM/DiagramModelSlot.inspector");
-	}
-
-	public void testDiagramPaletteInspector() {
-		validateFIB("Inspectors/VPM/DiagramPalette.inspector");
-	}
-
-	public void testDiagramPaletteElementInspector() {
-		validateFIB("Inspectors/VPM/DiagramPaletteElement.inspector");
-	}
-
-	public void testDiagramPatternRoleInspector() {
-		validateFIB("Inspectors/VPM/DiagramPatternRole.inspector");
-	}
-
-	public void testDiagramSpecificationInspector() {
-		validateFIB("Inspectors/VPM/DiagramSpecification.inspector");
-	}
-
-	public void testDiagramSpecificationResourceInspector() {
-		validateFIB("Inspectors/VPM/DiagramSpecificationResource.inspector");
-	}
-
+	@Test
 	public void testDropDownParameterInspector() {
 		validateFIB("Inspectors/VPM/DropDownParameter.inspector");
 	}
 
-	public void testDropSchemeInspector() {
-		validateFIB("Inspectors/VPM/DropScheme.inspector");
+	@Test
+	public void testFlexoBehaviourInspector() {
+		validateFIB("Inspectors/VPM/FlexoBehaviour.inspector");
 	}
 
+	@Test
+	public void testFlexoBehaviourParameterInspector() {
+		validateFIB("Inspectors/VPM/FlexoBehaviourParameter.inspector");
+	}
+
+	@Test
 	public void testFlexoConceptInspector() {
 		validateFIB("Inspectors/VPM/FlexoConcept.inspector");
 	}
 
+	@Test
 	public void testFlexoConceptInstanceParameterInspector() {
 		validateFIB("Inspectors/VPM/FlexoConceptInstanceParameter.inspector");
 	}
 
-	public void testFlexoConceptInstancePatternRoleInspector() {
+	@Test
+	public void testFlexoConceptInstanceRoleInspector() {
 		validateFIB("Inspectors/VPM/FlexoConceptInstanceRole.inspector");
 	}
 
+	@Test
 	public void testFlexoConceptObjectInspector() {
 		validateFIB("Inspectors/VPM/FlexoConceptObject.inspector");
 	}
 
-	public void testEditionSchemeInspector() {
-		validateFIB("Inspectors/VPM/FlexoBehaviour.inspector");
+	@Test
+	public void testFlexoRoleInspector() {
+		validateFIB("Inspectors/VPM/FlexoRole.inspector");
 	}
 
-	public void testEditionSchemeParameterInspector() {
-		validateFIB("Inspectors/VPM/FlexoBehaviourParameter.inspector");
-	}
-
-	public void testExampleDiagramInspector() {
-		validateFIB("Inspectors/VPM/ExampleDiagram.inspector");
-	}
-
-	public void testExampleDiagramConnectorInspector() {
-		validateFIB("Inspectors/VPM/ExampleDiagramConnector.inspector");
-	}
-
-	public void testExampleDiagramObjectInspector() {
-		validateFIB("Inspectors/VPM/ExampleDiagramObject.inspector");
-	}
-
-	public void testExampleDiagramShapeInspector() {
-		validateFIB("Inspectors/VPM/ExampleDiagramShape.inspector");
-	}
-
-	public void testFlexoModelObjectPatternRoleInspector() {
-		validateFIB("Inspectors/VPM/FlexoModelObjectPatternRole.inspector");
-	}
-
-	public void testFlexoObjectParameterInspector() {
-		validateFIB("Inspectors/VPM/FlexoObjectParameter.inspector");
-	}
-
+	@Test
 	public void testFloatParameterInspector() {
 		validateFIB("Inspectors/VPM/FloatParameter.inspector");
 	}
 
-	public void testGraphicalElementPatternRoleInspector() {
-		validateFIB("Inspectors/VPM/GraphicalElementPatternRole.inspector");
-	}
-
+	@Test
 	public void testIndividualParameterInspector() {
 		validateFIB("Inspectors/VPM/IndividualParameter.inspector");
 	}
 
+	@Test
 	public void testIndividualPatternRoleInspector() {
-		validateFIB("Inspectors/VPM/IndividualRole.inspector");
+		validateFIB("Inspectors/VPM/IndividualPatternRole.inspector");
 	}
 
+	@Test
 	public void testInnerModelSlotParameterInspector() {
 		validateFIB("Inspectors/VPM/InnerModelSlotParameter.inspector");
 	}
 
+	@Test
 	public void testIntegerParameterInspector() {
 		validateFIB("Inspectors/VPM/IntegerParameter.inspector");
 	}
 
-	public void testLinkSchemeInspector() {
-		validateFIB("Inspectors/VPM/LinkScheme.inspector");
-	}
-
+	@Test
 	public void testListParameterInspector() {
 		validateFIB("Inspectors/VPM/ListParameter.inspector");
 	}
 
+	@Test
 	public void testModelSlotInspector() {
 		validateFIB("Inspectors/VPM/ModelSlot.inspector");
 	}
 
+	@Test
 	public void testNamedViewPointObjectInspector() {
 		validateFIB("Inspectors/VPM/NamedViewPointObject.inspector");
 	}
 
+	@Test
 	public void testNavigationSchemeInspector() {
 		validateFIB("Inspectors/VPM/NavigationScheme.inspector");
 	}
 
+	@Test
 	public void testObjectPropertyParameterInspector() {
 		validateFIB("Inspectors/VPM/ObjectPropertyParameter.inspector");
 	}
 
+	@Test
 	public void testObjectPropertyPatternRoleInspector() {
-		validateFIB("Inspectors/VPM/ObjectPropertyRole.inspector");
+		validateFIB("Inspectors/VPM/ObjectPropertyPatternRole.inspector");
 	}
 
+	@Test
 	public void testOntologicObjectPatternRoleInspector() {
-		validateFIB("Inspectors/VPM/OntologicObjectRole.inspector");
+		validateFIB("Inspectors/VPM/OntologicObjectPatternRole.inspector");
 	}
 
-	public void testPatternRoleInspector() {
-		validateFIB("Inspectors/VPM/FlexoRole.inspector");
-	}
-
+	@Test
 	public void testPrimitivePatternRoleInspector() {
-		validateFIB("Inspectors/VPM/PrimitiveRole.inspector");
+		validateFIB("Inspectors/VPM/PrimitivePatternRole.inspector");
 	}
 
+	@Test
 	public void testPropertyParameterInspector() {
 		validateFIB("Inspectors/VPM/PropertyParameter.inspector");
 	}
 
+	@Test
 	public void testPropertyPatternRoleInspector() {
-		validateFIB("Inspectors/VPM/PropertyRole.inspector");
+		validateFIB("Inspectors/VPM/PropertyPatternRole.inspector");
 	}
 
-	public void testShapeOverridingGraphicalRepresentationInspector() {
-		validateFIB("Inspectors/VPM/ShapeOverridingGraphicalRepresentation.inspector");
-	}
-
-	public void testShapePatternRoleInspector() {
-		validateFIB("Inspectors/VPM/ShapePatternRole.inspector");
-	}
-
+	@Test
 	public void testSynchronizationSchemeInspector() {
 		validateFIB("Inspectors/VPM/SynchronizationScheme.inspector");
 	}
 
+	@Test
 	public void testTextAreaParameterInspector() {
 		validateFIB("Inspectors/VPM/TextAreaParameter.inspector");
 	}
 
+	@Test
 	public void testTextFieldParameterInspector() {
 		validateFIB("Inspectors/VPM/TextFieldParameter.inspector");
 	}
 
+	@Test
 	public void testTypeAwareModelSlotInspector() {
 		validateFIB("Inspectors/VPM/TypeAwareModelSlot.inspector");
 	}
 
+	@Test
 	public void testURIParameterInspector() {
 		validateFIB("Inspectors/VPM/URIParameter.inspector");
 	}
 
+	@Test
 	public void testViewPointInspector() {
 		validateFIB("Inspectors/VPM/ViewPoint.inspector");
 	}
 
+	@Test
 	public void testViewPointLibraryInspector() {
 		validateFIB("Inspectors/VPM/ViewPointLibrary.inspector");
 	}
 
+	@Test
 	public void testViewPointObjectInspector() {
 		validateFIB("Inspectors/VPM/ViewPointObject.inspector");
 	}
 
+	@Test
 	public void testViewPointResourceInspector() {
 		validateFIB("Inspectors/VPM/ViewPointResource.inspector");
 	}
 
+	@Test
 	public void testVirtualModelInspector() {
 		validateFIB("Inspectors/VPM/VirtualModel.inspector");
 	}
 
+	@Test
 	public void testVirtualModelModelSlotInspector() {
 		validateFIB("Inspectors/VPM/VirtualModelModelSlot.inspector");
 	}
 
+	@Test
 	public void testVirtualModelResourceInspector() {
 		validateFIB("Inspectors/VPM/VirtualModelResource.inspector");
 	}
