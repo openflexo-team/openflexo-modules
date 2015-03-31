37a1442d16a418d0e1b4cfa7194df606df169e7d
Sylvain Guérin <sylvain@openflexo.org>
2015-03-31 17:19:41 +0200

    IMPORTANT / InformationSpace module

---
+++ informationspacemodule/src/main/java/org/openflexo/ism/controller/ISMSelectionManager.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ * Copyright (c) 2013-2014, Openflexo
+ * Copyright (c) 2011-2012, AgileBirds
+ * 
+ * This file is part of Flexovieweditor, a component of the software infrastructure 
+ * developed at Openflexo.
+ * 
+ * 
+ * Openflexo is dual-licensed under the European Union Public License (EUPL, either 
+ * version 1.1 of the License, or any later version ), which is available at 
+ * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+ * and the GNU General Public License (GPL, either version 3 of the License, or any 
+ * later version), which is available at http://www.gnu.org/licenses/gpl.html .
+ * 
+ * You can redistribute it and/or modify under the terms of either of these licenses
+ * 
+ * If you choose to redistribute it and/or modify under the terms of the GNU GPL, you
+ * must include the following additional permission.
+ *
+ *          Additional permission under GNU GPL version 3 section 7
+ *
+ *          If you modify this Program, or any covered work, by linking or 
+ *          combining it with software containing parts covered by the terms 
+ *          of EPL 1.0, the licensors of this Program grant you additional permission
+ *          to convey the resulting work. * 
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. 
+ *
+ * See http://www.openflexo.org/license.html for details.
+ * 
+ * 
+ * Please contact Openflexo (openflexo-contacts@openflexo.org)
+ * or visit www.openflexo.org if you need additional information.
+ * 
+ */
+
+package org.openflexo.ism.controller;
+
+import java.util.logging.Logger;
+
+import org.openflexo.foundation.FlexoObject;
+import org.openflexo.selection.MouseSelectionManager;
+import org.openflexo.view.menu.FlexoMenuBar;
+
+/**
+ * This is the selection manager responsible for selection in ViewEditor module
+ * 
+ * @author sylvain
+ */
+public class ISMSelectionManager extends MouseSelectionManager {
+
+	protected static final Logger logger = Logger.getLogger(ISMSelectionManager.class.getPackage().getName());
+
+	public ISMSelectionManager(ISMController controller) {
+		super(controller);
+		FlexoMenuBar menuBar = controller.getMenuBar();
+		_contextualMenuManager = new ISMContextualMenuManager(this, controller);
+	}
+
+	public ISMController getISMController() {
+		return (ISMController) getController();
+	}
+
+	/**
+	 * Returns the root object that can be currently edited
+	 * 
+	 * @return FlexoModelObject
+	 */
+	@Override
+	public FlexoObject getRootFocusedObject() {
+		return getISMController().getCurrentDisplayedObjectAsModuleView();
+	}
+
+}
